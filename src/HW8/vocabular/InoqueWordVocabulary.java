14e7839d94c2a1ea2d0c5996b1a98776f8c21d09
jevgenijsjvf <jevgenijs@jvf.lv>
2019-03-23 00:51:19 +0200

    hw

---
+++ src/HW8/vocabular/InoqueWordVocabulary.java
@@ -0,0 +1,18 @@
+package HW8.vocabular;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InoqueWordVocabulary {
+
+//    В качестве коллекции использовать Set.
+//    Данный класс должен выполнять следующие задачи:
+//            1. Добавлять любую строку в коллекцию (addWord). Примечание: пустые строки не должны попасть в коллекцию.
+//            2. Получать (возвращать) количество уникальных слов (getUniqueWords)
+//            3. Выводить на консоль содержимое коллекции (printToConsole)
+
+    Set<String> vocabular = new HashSet<>();
+
+}
