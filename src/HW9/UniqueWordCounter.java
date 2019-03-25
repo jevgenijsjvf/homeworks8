e55bc52c307cc42e6596250f3a2c0676638f1045
jevgenijsjvf <jevgenijs@jvf.lv>
2019-03-26 00:42:32 +0200

    hw

--- src/HW9/UniqueWordCounter.java
+++ src/HW9/UniqueWordCounter.java
@@ -1,31 +1,46 @@
 package HW9;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UniqueWordCounter {
     Map <String,Integer >  stringOfWords = new HashMap<>();
-    int count;
+    int value=0;
 
-    public int addWord(String word) {
-        this.count=count;
-        count=0;
-        if (word !="") {
-            if (stringOfWords.containsKey(word)) {
-                    count++;
-            } stringOfWords.put(word,count);
-        }return count;
+
+    public int addWord(String key) {
+      int count=0;
+        if (key=="") {
+
+            System.out.println("Your string is Empty, they don't added to Map");
+       }
+        else {
+             if (stringOfWords.containsKey(key)) {
+                 count++;
+                 value=value+count;
+                   stringOfWords.put(key,value);
+            } else {
+                stringOfWords.put(key,value);}
+        }
+        return value;
+    }
+
+    public int findMaxValueInMap () {
+        int maxValueInMap = (Collections.max(stringOfWords.values()));
+         return maxValueInMap;
     }
 
     public void printToConsole() {
-        System.out.println("stringOfWords = " + stringOfWords);
-    }
-
-    public void returnFrequentlyEncounteredValue () {
-        for (Map.Entry<String,Integer> entry: stringOfWords.entrySet()) {
-
+           for (Map.Entry<String,Integer> entry: stringOfWords.entrySet()) {
+            if (entry.getValue() == findMaxValueInMap()) {
+                System.out.println(entry.getKey());
+            }
         }
 
     }
+
+
 
 }
--- src/HW9/UniqueWordCounterTest.java
+++ src/HW9/UniqueWordCounterTest.java
@@ -2,11 +2,26 @@ package HW9;
 
 public class UniqueWordCounterTest {
     public static void main(String[] args) {
+        UniqueWordCounterTest testRunner = new UniqueWordCounterTest();
+        testRunner.test1();
+    }
+
+    public void test1() {
         UniqueWordCounter counter = new UniqueWordCounter();
         counter.addWord("apple");
         counter.addWord("apple");
         counter.addWord("mango");
+        counter.addWord("apple");
+        counter.addWord("apple");
+        counter.findMaxValueInMap();
         counter.printToConsole();
-        counter.returnFrequentlyEncounteredValue();
+
+        int expectedResult = 3;
+        int actualResult = counter.findMaxValueInMap();
+        if (expectedResult==actualResult ) {
+            System.out.println("Test is correct.");
+        } else {
+            System.out.println("Test is wrong. Expected Max value in map must be: "+ expectedResult + " but, we have: " +expectedResult);
+        }
     }
 }
