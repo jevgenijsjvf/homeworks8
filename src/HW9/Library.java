51b42d0b0e307354e1c70f49aaafc6057f9e2258
jevgenijsjvf <jevgenijs@jvf.lv>
2019-03-28 01:01:03 +0200

    hw

--- src/HW9/Library.java
+++ src/HW9/Library.java
@@ -2,16 +2,43 @@ package HW9;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Library{
-   // Book book;
 
-    List<String> books = new ArrayList<>();
-
-
-//    public void addBook(Book book) {
-//        books.add(String.valueOf(library));
-//    }
+   private List<Book> books = new ArrayList<>();
 
 
+   public void createBook (Book book) {
+
+      books.add(book);
+   }
+   public void removeBookFFromLibrary() {}
+   public void findBookInLibraryByTitle() {}
+   public void findBookInLibraryByAuthor () {}
+
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Library)) return false;
+      Library library = (Library) o;
+      return Objects.equals(books, library.books);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(books);
+   }
+
+   public List<Book> getBooks() {
+      return books;
+   }
+
+   @Override
+   public String toString() {
+      return "Library{" +
+              "books=" + books +
+              '}';
+   }
 }
--- src/HW9/LibraryService.java
+++ src/HW9/LibraryService.java
@@ -1,11 +1,19 @@
 package HW9;
 
+import com.sun.jmx.mbeanserver.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import HW9.Library;
 
 public class LibraryService {
     public static void main(String[] args) {
+
         Library library = new Library();
-        //library.addBook("a","a");
-        System.out.println(library.books);
+
+      //  library.createBook("Autors","Day after tomorrow");
+
+        System.out.println(library.getBooks());
     }
 }
