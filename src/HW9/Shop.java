07d72b7e51c5f70794e1da44b7b9291e7e932751
jevgenijsjvf <jevgenijs@jvf.lv>
2019-03-30 03:24:53 +0200

    hw

--- src/HW9/Shop.java
+++ src/HW9/Shop.java
@@ -2,6 +2,7 @@ package HW9;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Shop {
@@ -10,22 +11,37 @@ public class Shop {
     public void addProduct(Product product) {
         products.add(product);
     }
-    public void addListOfProducts(Product product) {
-        products.addAll(products);
+    public void addListOfProducts(ArrayList items) {
+        //Collections<Product producti> col=new ArrayList<String>();
+        products.addAll(items);
     }
-    public void removeProductByName (Product product) {
-        products.remove(product);
+    public void removeProductByName (String name) {
+        for (int i =0;i<products.size();i++ ) {
+            products.get(i).getName();
+            if (products.get(i).getName().equals(name)) {
+                System.out.println("Product which you are remove: "+products.get(i));
+                products.remove(products.get(i));
+            }
+        }
     }
+
     public void getProductByName (String name) {
         for (int i =0;i<products.size();i++ ) {
             products.get(i).getName();
             if (products.get(i).getName().equals(name)) {
                 System.out.println("Product which you are search: "+products.get(i));
             }
+
         }
     }
     public void findProductInPriceRange (BigDecimal minPrice, BigDecimal maxPrice) {
+        for (int i =0;i<products.size();i++ ) {
+            BigDecimal x = products.get(i).getPrice();
+            if ((x.compareTo(maxPrice) <=0) && (x.compareTo(minPrice) >=0)) {
+                System.out.println("Product which are in price range: "+products.get(i));
+            }
 
+        }
     }
 }
 
--- src/HW9/ShopService.java
+++ src/HW9/ShopService.java
@@ -1,22 +1,48 @@
 package HW9;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShopService {
     public static void main(String[] args) {
+        ShopService RIMI = new ShopService();
+        RIMI.test1();
+
+    }
+
+    public void test1() {
         Shop shop=new Shop();
         Product bread=new Product("Bread",new BigDecimal("1.56"));
         shop.addProduct(bread);
-        System.out.println(shop.products);
-
+        Product water=new Product("water",new BigDecimal("0.5"));
+        shop.addProduct(water);
         Product butter=new Product("Butter",new BigDecimal("2.06"));
-        Product CocaCola=new Product("Coca Cola 0.5 L",new BigDecimal("1.20"));
-        Product bear=new Product("Tervetes alus 0.5 L",new BigDecimal("1.42"));
-        Product whisky=new Product("Ballantimes",new BigDecimal("25.12"));
-        for (int i =0 ; i<4; i++) {
-
-        }
-        shop.addListOfProducts(bear);
+        shop.addProduct(butter);
         System.out.println(shop.products);
+        shop.removeProductByName("water");
+        System.out.println(shop.products);
+        shop.getProductByName("Bread");
+        shop.findProductInPriceRange(new BigDecimal("1.00"),new BigDecimal("3.0"));
+        List<Product> productList = new ArrayList<>();
+        Product CocaCola=new Product("Coca Cola 0.5 L",new BigDecimal("1.20"));
+        productList.add(CocaCola);
+        Product bear=new Product("Tervetes alus 0.5 L",new BigDecimal("1.42"));
+        productList.add(bear);
+        Product whisky=new Product("Ballantimes",new BigDecimal("25.12"));
+        productList.add(whisky);
+        System.out.println(productList);
+        shop.addListOfProducts((ArrayList) productList);
+        System.out.println(shop.products);
+        System.out.println("*********************************************");
+        int expectedResult = 5;
+        int actualResult = shop.products.size();
+        if (expectedResult==actualResult) {
+            System.out.println("TEST CORRECT size of List are equal "+expectedResult);
+            }
+        else {
+            System.out.println("Test WRONG. Expected size of list must be - "+expectedResult+" but we receive: "+actualResult);
+        }
+
     }
 }
